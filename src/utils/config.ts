168ed121f75aff8b336c4deeecf0ff50dd4ee171
Tuan Pham <58241974+tuanp20@users.noreply.github.com>
2023-12-26 18:10:20 +0700

    config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,2 +1,4 @@
-export const TMDB_API_BASE_URL: string | undefined = import.meta.env.VITE_TMDB_API_BASE_URL
- export const API_KEY: string | undefined = import.meta.env.VITE_API_KEY 
+// export const TMDB_API_BASE_URL: string | undefined = import.meta.env.VITE_TMDB_API_BASE_URL
+//  export const API_KEY: string | undefined = import.meta.env.VITE_API_KEY 
+export const TMDB_API_BASE_URL: string | undefined = "https://api.themoviedb.org/3"
+ export const API_KEY: string | undefined = "b231bd13e1ecfd21838e2c632a6ed9c1"
